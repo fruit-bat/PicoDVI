baf1e2ba1cd46dfa1e4c090a0dbea2bacc5bd3ca
Phil <pmscull@gmail.com>
2024-06-17 22:13:54 +0100

    PackedMidi

--- software/apps/scratch_audio/u_packed_midi.c
+++ software/apps/scratch_audio/u_packed_midi.c
@@ -1,3 +1,23 @@
 #include "u_packed_midi.h"
 
 
+UsPmCursor us_packed_midi_step(
+    UsPmCursor cursor,
+    UsPmMessage *message
+) {
+    if (cursor == NULL) return cursor;
+    const uint8_t type = *cursor++;
+    message->type = type;
+    switch(type) {
+        case SynCmdPPQ:
+        case SynCmdTempo:
+        case SynCmdOn:
+        case SynCmdOff:
+        case SynCmdTime:
+        default:
+        case SynCmdEnd:
+        break;
+    }
+
+    return cursor;
+}
--- software/apps/scratch_audio/u_packed_midi.h
+++ software/apps/scratch_audio/u_packed_midi.h
@@ -1,1 +1,50 @@
 #pragma once
+#include "pico/stdlib.h"
+
+enum UsPmCommands {
+    SynCmdPPQ,
+    SynCmdTempo,
+    SynCmdOn,
+    SynCmdOff,
+    SynCmdTime,
+    SynCmdEnd
+};
+
+typedef uint8_t * UsPmCursor;
+
+typedef struct {
+    uint8_t voice;
+    uint8_t key;
+    uint8_t velocity;
+} UsPmNoteOn;
+
+typedef struct {
+    uint8_t voice;
+    uint8_t velocity;
+} UsPmNoteOff;
+
+typedef struct {
+    uint16_t ticks;
+} UsPmTickDelta;
+
+typedef struct {
+    uint16_t ppq;
+} UsPmPpq;
+
+typedef struct {
+    uint32_t tempo;
+} UsPmTempo;
+
+typedef struct {
+    uint8_t type;
+    union
+    {
+        UsPmNoteOn note_on;
+        UsPmNoteOff note_off;
+        UsPmTickDelta tick_delta;
+        UsPmPpq ppq;
+        UsPmTempo tempo;
+    } data;
+    
+} UsPmMessage;
+
