e5d563bd863a060596e967626d0d83a0e38afb54
Phil <pmscull@gmail.com>
2024-06-23 13:39:19 +0100

    half wave

--- software/apps/scratch_audio/us_wave.c
+++ software/apps/scratch_audio/us_wave.c
@@ -30,6 +30,15 @@ inline static int32_t us_wave_quater_sample_q(
     }
 }
 
+inline static int32_t us_wave_half_sample_h(
+    const uint32_t hang,
+    const uint32_t hind,
+    const uint16_t* samples,
+    const uint32_t sample_count
+) {
+    return hang ? -samples[sample_count - hind] : samples[hind];
+}
+
 inline static int32_t us_wave_quarter_sample(
     const uint32_t sang,
     const uint16_t* samples,
@@ -39,6 +48,17 @@ inline static int32_t us_wave_quarter_sample(
     const uint32_t qang = sang >> sample_count_ln2;
     const uint32_t qind = sang & (sample_count - 1L);
     return us_wave_quater_sample_q(qang, qind, samples, sample_count);
+}
+
+inline static int32_t us_wave_half_sample(
+    const uint32_t sang,
+    const uint16_t* samples,
+    const uint32_t sample_count_ln2
+) {
+    const int32_t sample_count = 1L << sample_count_ln2;
+    const uint32_t qang = sang >> sample_count_ln2;
+    const uint32_t qind = sang & (sample_count - 1L);
+    return us_wave_half_sample_h(qang, qind, samples, sample_count);
 }
 
 inline static int32_t us_wave_quarter_lerp(
@@ -52,6 +72,20 @@ inline static int32_t us_wave_quarter_lerp(
     const int32_t s1 = us_wave_quarter_sample(sang1, samples, sample_count_ln2);
     const uint32_t sang2 = (sang1 + 1) & ((1 << (2 + sample_count_ln2)) - 1);
     const int32_t s2 = us_wave_quarter_sample(sang2, samples, sample_count_ln2);
+    return us_lerp_n(s1, s2, fang, 8);
+}
+
+inline static int32_t us_wave_half_lerp(
+    const uint32_t bang,
+    const uint16_t* samples,
+    const uint32_t sample_count_ln2
+) {
+    const uint32_t zang = bang >> (32 - (1 + sample_count_ln2 + 8));
+    const uint32_t fang = zang & 255;
+    const uint32_t sang1 = zang >> 8;
+    const int32_t s1 = us_wave_half_sample(sang1, samples, sample_count_ln2);
+    const uint32_t sang2 = (sang1 + 1) & ((1 << (1 + sample_count_ln2)) - 1);
+    const int32_t s2 = us_wave_half_sample(sang2, samples, sample_count_ln2);
     return us_lerp_n(s1, s2, fang, 8);
 }
 
@@ -102,6 +136,7 @@ int32_t __not_in_flash_func(us_wave_ramp_down)(
     return qang < 0 ? 0 : qang;
 }
 
+// Example quarter wave samples for lerping
 const uint16_t us_wave_not_square_samples[] = {
     0,
     15000,
@@ -110,8 +145,22 @@ const uint16_t us_wave_not_square_samples[] = {
     32767
 };
 
+// Example quarter wave samples for lerping
+const uint16_t us_wave_half_not_square_samples[] = {
+    0,
+    15000,
+    17000,
+    30000,
+    32000,
+    32000,
+    20000,
+    10000,
+    400
+};
+
 int32_t __not_in_flash_func(us_wave_not_square_lerp)(
     const uint32_t bang
 ) {
-    return us_wave_quarter_lerp(bang, us_wave_not_square_samples, 2);
+    //return us_wave_half_lerp(bang, us_wave_not_square_samples, 2);
+    return us_wave_half_lerp(bang, us_wave_half_not_square_samples, 3);
 }
